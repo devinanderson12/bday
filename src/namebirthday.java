7c1c48339bb984d8f77712270c816fb433260016
devinanderson12 <devinanderson142@gmail.com>
2016-12-16 09:52:15 -0600

    A6, Q1
    A6, Q1

---
+++ src/namebirthday.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+
+/**
+ * @author d.anderson2
+ *
+ */
+public class namebirthday {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		displayName();
+        displayBirthDay();
+        displayFact();
+
+	}
+	/**
+	 * @param there are no parameters.
+     * @return Nothing will be returned.
+	 */
+	public static void displayName(){
+		System.out.println("Devin");
+	}
+	/**
+	 * @param there are no parameters.
+     * @return Nothing will be returned.
+	 */
+	public static void displayBirthDay(){
+		System.out.println("July 16,2001");
+	}
+	/**
+	 * @param there are no parameters.
+     * @return Nothing will be returned.
+	 */
+	public static void displayFact(){
+		System.out.println("i am good wiht numbers");
+	}
+	
+}
+
